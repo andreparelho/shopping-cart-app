afc7077c0d12b850d5dcabb6eb38323ca1cc1237
Andre <parelhoneves.andre@gmail.com>
2023-11-04 12:23:55 -0300

    update application

--- shopping-cart.app/src/main/java/com/shoppingcart/app/Application.java
+++ shopping-cart.app/src/main/java/com/shoppingcart/app/Application.java
@@ -1,28 +1,12 @@
 package com.shoppingcart.app;
 
-import com.shoppingcart.app.httpClient.Currency.CurrencyGateway;
-import com.shoppingcart.app.httpClient.Currency.CurrencyGatewayImpl;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application {
 
-	private final CurrencyGateway currencyGateway;
-	private final CurrencyGatewayImpl currencyGatewasy;
-
-	public Application(CurrencyGateway currencyGateway, CurrencyGatewayImpl currencyGatewasy) {
-		this.currencyGateway = currencyGateway;
-		this.currencyGatewasy = currencyGatewasy;
-	}
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println(currencyGatewasy.getCurrency("BRL"));
 	}
 }
