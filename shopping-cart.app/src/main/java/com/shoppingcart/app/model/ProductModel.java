4ebb9e59ed035ba13bc952d59729cfaaf62e3564
Andre <parelhoneves.andre@gmail.com>
2023-11-04 13:25:43 -0300

    add product model to app

---
+++ shopping-cart.app/src/main/java/com/shoppingcart/app/model/ProductModel.java
@@ -0,0 +1,33 @@
+package com.shoppingcart.app.model;
+
+import java.math.BigDecimal;
+
+public class ProductModel {
+    private int productId;
+    private String description;
+    private BigDecimal amount;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
