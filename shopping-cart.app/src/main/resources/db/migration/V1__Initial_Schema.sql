6581e643d3954b95715a2729929dab1ffef67b23
Andre <parelhoneves.andre@gmail.com>
2023-11-04 13:14:03 -0300

    add migration to initial database

---
+++ shopping-cart.app/src/main/resources/db/migration/V1__Initial_Schema.sql
@@ -0,0 +1,12 @@
+create table products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    price DECIMAL(10, 2)
+);
+
+insert into products values (1, 'A', 20);
+insert into products values (2, 'B', 54.50);
+insert into products values (3, 'C', 900);
+insert into products values (4, 'D', 500);
+insert into products values (5, 'E', 300);
+insert into products values (6, 'F', 129);
